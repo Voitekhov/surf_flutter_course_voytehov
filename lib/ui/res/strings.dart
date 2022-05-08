eaa30baf48d340353c97534b45cbcf98a81ea792
antonvoitekhov <ads>
2022-05-08 16:22:09 +0500

    hw 4.2 style AppBar and add styling resource

---
+++ lib/ui/res/strings.dart
@@ -0,0 +1,8 @@
+
+class AppStrings {
+  static const String appBarTitle = 'Список\nинтересных мест';
+}
+
+
+
+
---
+++ lib/ui/res/text_style_templates.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:places/ui/res/fonts';
+import 'package:places/ui/res/app_colors.dart';
+
+class TextStyleTemplates {
+  static const TextStyle appBar = TextStyle(
+    color: AppColors.defaultTextColor,
+    fontWeight: FontWeight.bold,
+    fontFamily: FontFamily.defaultFontFamily,
+    fontSize: Fonts.defaultAppBarTitle,
+  );
+}
