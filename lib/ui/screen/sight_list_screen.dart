aac2587d341d237bbc85052456d46caa0b7be3ea
antonvoitekhov <ads>
2022-05-19 19:24:42 +0500

    hw4.7 experiments with PrefferedSize, AspectRatio and SizedBox

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:places/mocks.dart';
-import 'package:places/ui//res/strings.dart';
-import 'package:places/ui/res/app_colors.dart';
-import 'package:places/ui/res/text_style_templates.dart';
+import 'package:places/ui/screen/CustomAppBar.dart';
 import 'package:places/ui/screen/sight_card.dart';
+import 'package:places/mocks.dart';
 
 class SightListScreen extends StatefulWidget {
   const SightListScreen({Key? key}) : super(key: key);
@@ -16,23 +14,15 @@ class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColors.backgroundColor,
-        title: const Align(
-          alignment: Alignment.centerLeft,
-          child: Text(
-            AppStrings.appBarTitle,
-            style: TextStyleTemplates.appBar,
-          ),
-        ),
-        elevation: 0,
-      ),
+      appBar: MyAppBar.createAppBarUsingPreferredSize(),
       body: ListView.builder(
-          itemBuilder: (context, index) {
-            return SightCard(sight: MockData.sights[index]);
-          },
-          itemCount: MockData.sights.length,
-        ),
+        itemBuilder: (context, index) {
+          return AspectRatio(
+              aspectRatio: 3 / 2,
+              child: SightCard(sight: MockData.sights[index]));
+        },
+        itemCount: MockData.sights.length,
+      ),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
